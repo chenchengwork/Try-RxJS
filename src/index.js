ef4361fee45116ea5f90f65f0590fd2839411271
chencheng <dainel_txy@163.com>
2018-10-18 20:06:55 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,37 @@ import { Observable, fromEvent } from 'rxjs';
 import { throttleTime, scan } from 'rxjs/operators';
 
 const observable = Observable.create(function (observer) {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
+    console.log('1111111111')
+    observer.next(1, "a");
+    observer.next(2, "b");
+    observer.next(3, "c");
     setTimeout(() => {
         observer.next(4);
         observer.complete();
     }, 1000);
 });
+console.log(observable)
+// const observer =
+// observable.subscribe({
+//     next: x => console.log('got value ' + x),
+//     error: err => console.error('something wrong occurred: ' + err),
+//     complete: () => console.log('done'),
+// });
+
 
 const button = document.querySelector('button');
 const clickObservable = fromEvent(button, 'click');
 
-const subscription = clickObservable.pipe(throttleTime(1000))
-    .pipe(scan(count => count + 1, 0))
-    .subscribe(count => {
+const subscription = clickObservable.pipe()
+    .pipe(
+        throttleTime(1000),
+        scan((count) => {
+            return count + 1
+        }, 0)
+    )
+    .subscribe((count) => {
         console.log(`Clicked ${count} times`);
-        subscription.unsubscribe();
+        // subscription.unsubscribe();
     });
 
 
